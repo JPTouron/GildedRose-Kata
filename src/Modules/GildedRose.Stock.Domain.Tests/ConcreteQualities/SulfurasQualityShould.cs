973a6584546f38c83a2f1333f5c1c1df869badb0
JP <jp.touron@gmail.com>
2021-03-20 21:47:09 +0100

    implemented and tested sulfuras quality
    On branch Refactors/Take1

---
+++ src/Modules/GildedRose.Stock.Domain.Tests/ConcreteQualities/SulfurasQualityShould.cs
@@ -0,0 +1,42 @@
+﻿using GildedRose.Stock.Domain.Qualities;
+using GildedRose.Stock.Domain.ValueObjects;
+using System;
+using Xunit;
+
+namespace GildedRose.Stock.Domain.ConcreteQualities
+{
+    public class SulfurasQualityShould
+    {
+        [Theory]
+        [InlineData(new object[] { 0 })]
+        [InlineData(new object[] { 8 })]
+        [InlineData(new object[] { 1 })]
+        [InlineData(new object[] { 10 })]
+        [InlineData(new object[] { 50 })]
+        public void BeCreatedSuccesfully_AndHoldItsInitialQualityAndSellInValue(int initialQualityValue)
+        {
+            var q = CreateQuality(initialQualityValue);
+
+            Assert.Equal(initialQualityValue, q.Value);
+        }
+
+        [Fact]
+        internal void Throw_WhenQualityIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new SulfurasQuality(null));
+        }
+
+        private static QualityValue CreateQualityValue(int initialQualityValue)
+        {
+            return new QualityValue(initialQualityValue);
+        }
+
+        private SulfurasQuality CreateQuality(int initialQualityValue)
+        {
+            var qv = CreateQualityValue(initialQualityValue);
+
+            var q = new SulfurasQuality(qv);
+            return q;
+        }
+    }
+}
---
+++ src/Modules/GildedRose.Stock.Domain/Qualities/SulfurasQuality.cs
@@ -0,0 +1,19 @@
+﻿using Ardalis.GuardClauses;
+using GildedRose.Stock.Domain.Qualities.Contracts;
+using GildedRose.Stock.Domain.ValueObjects;
+
+namespace GildedRose.Stock.Domain.Qualities
+{
+    public class SulfurasQuality : IQuality
+    {
+        private readonly QualityValue currentQuality;
+
+        public SulfurasQuality(QualityValue initialQuality)
+        {
+            Guard.Against.Null(initialQuality, nameof(initialQuality));
+            currentQuality = initialQuality;
+        }
+
+        public int Value => currentQuality.Value;
+    }
+}
