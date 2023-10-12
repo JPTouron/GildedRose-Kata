954726b38f61fae9f2cce159d4360f59f4eabc98
JP <juan.touron@novafori.com>
2023-10-12 13:40:33 +0200

    completed unit test for initial console state using a home made of approval testing, where you test your result against a specific and expected text output

---
+++ src/GildedRose.Tests/ConsoleOutputRedirect.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace GildedRose.Tests;
+
+/// <summary>
+/// this class gets the output of a console from a specific program and puts it into a stringwriter for later review within a test for example
+/// </summary>
+public class ConsoleOutputRedirect : IDisposable
+{
+    private StringWriter stringWriter;
+    private TextWriter originalOutput;
+
+    public ConsoleOutputRedirect()
+    {
+        stringWriter = new StringWriter();
+        originalOutput = System.Console.Out;
+        System.Console.SetOut(stringWriter);
+    }
+
+    public string GetOutput()
+    {
+        return stringWriter.ToString();
+    }
+
+    public void Dispose()
+    {
+        stringWriter.Dispose();
+        System.Console.SetOut(originalOutput);
+    }
+}
--- src/GildedRose.Tests/UnitTest1.cs
+++ src/GildedRose.Tests/UnitTest1.cs
@@ -1,13 +1,37 @@
+using System.IO;
+using GildedRose.Console;
 using Xunit;
 
-namespace GildedRose.Tests
+namespace GildedRose.Tests;
+
+public class ProgramTests
 {
-    public class UnitTest1
+    [Fact]
+    public void FirstTest()
     {
-        [Fact]
-        public void FailingTest()
+        //arrange
+        using (var consoleRedirect = new ConsoleOutputRedirect())
         {
-            Assert.True(false);
+            //act
+            Program.Main(new string[] { });
+            var actualConsoleOutput = consoleRedirect.GetOutput();
+
+            //assert
+            //we'll use approval testing techinque to verify the state with super minimum changes in productive code
+            string expectedConsoleOutput = GetExpectedConsoleOutput();
+            Assert.Contains(expectedConsoleOutput, actualConsoleOutput);
         }
+    }
+
+    private static string GetExpectedConsoleOutput()
+    {
+        //we ran the productive code once, and we took note of the output within this file,
+        //and we accept it as the valid result, from there, we just contrast results against it to know if it's ok or not
+        //info: https://approvaltests.com/
+        //info: https://en.wikipedia.org/wiki/Acceptance_testing
+        var filePath = "ProgramValidResult.txt";
+
+        var expectedFileContents = File.ReadAllText(filePath);
+        return expectedFileContents;
     }
 }
